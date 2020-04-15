dd29ee34dd01c8dd8f9842d4aeca5e8ac93ece44
Anna Krylova <annakry@wix.com>
2020-04-15 14:05:49 +0300

    Init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Preview from './Preview';
+import "./index.css" 
+
+ReactDOM.render(
+
+  <React.StrictMode>
+    <Preview />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
